018a44bd4564785065ff48c911154b0aae06e031
Miguel <migtosi99@gmail.com>
2022-05-16 14:45:22 -0300

    Criação do arquivo js do 2DayOfCode

---
+++ 2dayOfCode.js
@@ -0,0 +1,24 @@
+
+let nome =  prompt("Qual o seu nome?")
+let idade = prompt("Quantos anos você tem?")
+let linguagem = prompt("Qual linguagem de programação você está estudando?")
+
+document.write("Olá " + nome + ", você tem " + idade + " anos e já está aprendendo " + linguagem + "!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
